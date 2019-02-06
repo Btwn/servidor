4cbb15d792f16a196a4a90e8ad3ba4e0f99a3401
Carlos A. Diaz Jimenez <ca.diaz@mueblesamerica.mx>
2019-02-06 17:50:39 -0600

    Configurando vuetify

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,13 @@
 	<div>
 		<h2>Index</h2>
 		<router-link to="/about">Acerca de esta paginas</router-link>
+		<br>
+		<v-content>
+			
+
+				<v-btn color="success">Success</v-btn>
+
+		</v-content>
 	</div>
 </template>
 
