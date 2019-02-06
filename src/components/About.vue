8ba097bb069d92759da4c393c942771745755244
Carlos A. Diaz Jimenez <ca.diaz@mueblesamerica.mx>
2019-02-06 16:24:44 -0600

    configuracion de vue-router

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h2>About</h2>
+		<router-link to="/about">Acerca de esta paginas</router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'About'
+}
+</script>
+
+<style>
+
+</style>
