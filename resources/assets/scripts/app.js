c7694f31564da5b749f09cad3a809720bc2a134f
Carlos A. Diaz Jimenez <ca.diaz@mueblesamerica.mx>
2019-02-06 10:31:00 -0600

    añadido vuex

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import router from './router'
 import App from '@/components/layout/App.vue'
+import store from '@/store/index.js'
 
 Vue.config.productionTip = false
 
 new Vue({
 	el: '#app',
 	router,
+	store,
 	// components: { App },
 	render: h => h(App),
 	template: '<App/>'
--- resources/assets/scripts/components/Index.vue
+++ resources/assets/scripts/components/Index.vue
@@ -2,12 +2,28 @@
 	<div>
 		<h2>Index</h2>
 		<router-link to="/about">Acerca de esta paginas</router-link>
+		<p>{{ count }}</p>
+		<input type="button" value="incrementa" @click.prevent="increment">
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'Index'
+	name: 'Index',
+	computed: {
+		count: function() {
+			return this.$store.state.count
+		}
+	},
+	methods: {
+		increment: function() {
+			this.$store.commit('increment')
+		}
+	},
+	created() {
+		
+		console.log(this.$store.state.count)
+	}
 }
 </script>
 
