33f15f600b9ad976247a0ce60fe4ced770148e58
Carlos A. Diaz Jimenez <ca.diaz@mueblesamerica.mx>
2019-02-06 18:04:53 -0600

    configurando vuex

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,14 @@
 <template>
 	<div>
 		<h2>Index</h2>
-		<router-link to="/about">Acerca de esta paginas</router-link>
+		<router-link to="/about">Acerca de esta paginassq</router-link>
 		<br>
-		<v-content>
+
 			
 
 				<v-btn color="success">Success</v-btn>
 
-		</v-content>
+
 	</div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
