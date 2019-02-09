384c35d1ba5679cfb29e72addb4d1a2f402f4682
Carlos A. Diaz Jimenez <carlos4diazj@hotmail.com>
2019-02-08 19:22:09 -0600

    cambio en el nombre del controlador ApiController a PathController

--- app/Tools/Path/nomenclaturaMavi.js
+++ app/Tools/Path/nomenclaturaMavi.js
@@ -3,12 +3,19 @@ const path = require('path')
 /***
  * Función que crea una nomenclatura que se usara como nombre de archivo
  * Ejemplo: recive Archivo.frm y retorna en Archivo_FRM_MAVI.esp
- * @arreglo Contiene los nombres que seran transformados
+ * @param {string} nombre Contiene los nombres que seran transformados
+ * @return {string}
  ***/
 PathNombreToMavi = nombre => {
 	return nombre.split('.').map((x,k) => k==1 ? x.toUpperCase() : x).join('_') + '_MAVI.esp'
 }
 
+/***
+ * Función que toma la nomenclatura de nombrea de archivos y lo reconstruye al nombre original
+ * Ejemplo: recive Archivo_FRM_MAVI.esp y retorna en Archivo.frm
+ * @param {string} nombre Contiene los nombres que seran transformados
+ * @return {string}
+ ***/
 PathMaviToNombre = nombre => {
 	let cambio = nombre.split('_')
 	cambio.pop()
